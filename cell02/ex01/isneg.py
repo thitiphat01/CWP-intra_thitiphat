34f4d6e4f27a6a8bc4a53f6e94bfdfd1d8a3ca4e
thitiphat01 <67070043@kmitl.ac.th>
2026-09-21 23:18:40 +0700

    cell02

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+number = int(input())
+
+if number < 0:
+    print("This number is negative.")
+elif number > 0:
+    print("This number is positive.")
+else:
+    print("This number is both positive and negative.")
---
+++ cell02/ex02/password.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+password = "Python is awesome"
+
+user_password = input()
+
+if user_password == password:
+    print("ACCESS GRANTED")
+else:
+    print("ACCESS DENIED")
